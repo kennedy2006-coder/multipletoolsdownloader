0781482b5ec815d58237e731b2f11adc3bc2a909
kennedy2006-coder <kennypastino16@gmail.com>
2026-03-14 14:41:00 +0100

    Update summarize.js

--- api/summarize.js
+++ api/summarize.js
@@ -2,23 +2,24 @@ export default async function handler(req, res) {
 
 const { text } = req.body;
 
-const apiKey = "AIzaSyA9m8az9N6e657qSEjbyp-08BakDqF4zNA";
+const apiKey = "YOUR_API_KEY";
 
 try {
 
 const response = await fetch(
 `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
 {
-method: "POST",
-headers: {
-"Content-Type": "application/json"
+method:"POST",
+headers:{
+"Content-Type":"application/json"
 },
-body: JSON.stringify({
-contents: [
+body:JSON.stringify({
+contents:[
 {
-parts: [
+role:"user",
+parts:[
 {
-text: `Summarize these study notes clearly for a university student:\n\n${text}`
+text:`Summarize these study notes clearly:\n\n${text}`
 }
 ]
 }
@@ -31,21 +32,21 @@ const data = await response.json();
 
 console.log(data);
 
-if(data.candidates && data.candidates.length > 0){
+if(data.candidates){
 
 const summary = data.candidates[0].content.parts[0].text;
 
-res.status(200).json({ summary });
+res.status(200).json({summary});
 
 }else{
 
-res.status(200).json({ summary:"AI could not generate a summary." });
+res.status(200).json({summary:"AI could not generate a summary."});
 
 }
 
-} catch (error) {
+}catch(error){
 
-res.status(500).json({ error: "AI summarization failed" });
+res.status(500).json({error:"Server error"});
 
 }
 
